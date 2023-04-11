bf8a36830b2ce10aae12ce0c9eccce9ca5ce3e83
Alina <pavlenkoalina9694@gmail.com>
2023-04-11 21:55:21 +0300

    fixed basename

--- src/App.js
+++ src/App.js
@@ -47,34 +47,35 @@ export const AdminPrivateRoute = ({ children }) => {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="drivers/signup" element={<SignUpScreen />}></Route>
-      <Route path="drivers/login" element={<LoginScreen />}></Route>
-      <Route path="drivers/phone" element={<PhoneLogin />}></Route>
+      <Route path="/signup" element={<SignUpScreen />}></Route>
+      <Route path="/login" element={<LoginScreen />}></Route>
+      <Route path="/phone" element={<PhoneLogin />}></Route>
       <Route
-        path="drivers/"
+        path="/"
         element={
           <PrivateRoute>
             <RootLayout />
           </PrivateRoute>
         }
       >
-        <Route path="drivers/" element={<MainScreen />} />
-        <Route path="drivers/profile" element={<ProfileScreen />} />
-        <Route path="drivers/create" element={<CreateTrip />} />
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/create" element={<CreateTrip />} />
       </Route>
       <Route
-        path="drivers/admin"
+        path="/admin"
         element={
           <AdminPrivateRoute>
             <RootLayout />
           </AdminPrivateRoute>
         }
       >
-        <Route path="drivers/admin" element={<AdminPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Route>
       <Route path="*" element={<NotFoundPage />}></Route>
     </>
-  )
+  ),
+  { basename: "/drivers" }
 );
 
 function App() {
